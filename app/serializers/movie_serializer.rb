15604214957fdd0193b1305769ba5acc31834aae
Wes Schierenbeck <wschierenbeck@gmail.com>
2021-12-15 16:58:33 -0500

    commit tree 2254a0d52dedb1f31d94d376fb9257c4cbb7647b

--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -1,4 +1,12 @@
 class MovieSerializer < ActiveModel::Serializer
-  attributes :id, :title, :year, :length, :description, :poster_url, :category, :discount
-  
+  attributes :id,
+             :title,
+             :year,
+             :length,
+             :description,
+             :poster_url,
+             :category,
+             :discount
+
+  belongs_to :director
 end
